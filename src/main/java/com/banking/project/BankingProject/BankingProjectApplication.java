f0d190c1a7bef945cd2eadaecfba1a6e28357f1a
vishu568 <vishu.568@gmail.com>
2025-05-19 20:41:13 +0530

    Removed SysOut and comments for Push Demo

--- src/main/java/com/banking/project/BankingProject/BankingProjectApplication.java
+++ src/main/java/com/banking/project/BankingProject/BankingProjectApplication.java
@@ -9,8 +9,6 @@ public class BankingProjectApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(BankingProjectApplication.class, args);
-
-		System.out.println("Hello World");
 	}
 
 }
--- src/main/java/com/banking/project/BankingProject/controller/HelloWorldController.java
+++ src/main/java/com/banking/project/BankingProject/controller/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-// HelloWorld API
+
     //http://localhost:8080/hello-world
     @GetMapping("/hello-world")
     public String helloWorld(){
